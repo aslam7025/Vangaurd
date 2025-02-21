048433e795f01d375aa74b2c4ab2ab7d13f487c8
aslam <hafizaslam@gmail.com>
2025-02-21 21:06:13 +0530

    Removed .env from tracking

---
+++ config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+const env = require('dotenv').config()
+
+ 
+const connectDB = async()=>{
+    try{
+
+        await mongoose.connect(process.env.MONGODB_URI)
+        console.log("DB Connected")
+
+    }catch(err){
+        console.error('DB connected error',err.message)
+        process.exit(1)
+
+    }
+}
+
+
+module.exports = connectDB
