28cadf983347af92d7a4ddc2502649dab704845e
aslam <hafizaslam@gmail.com>
2025-04-16 14:03:17 +0530

    this will work

--- config/passport.js
+++ config/passport.js
@@ -9,7 +9,7 @@ const User = require('../models/userSchema')
 passport.use(new GoogleStrategy({
     clientID:process.env.GOOGLE_CLIENT_ID,
     clientSecret:process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: 'https://vanguard.today/auth/google/callback'
+    callbackURL: 'https://vangaurd.today/auth/google/callback'
 ,
     
 },
