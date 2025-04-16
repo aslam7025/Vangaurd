6b235ea2e0b2e07a9609b5d2100cf592f8616ded
aslam <hafizaslam@gmail.com>
2025-04-16 13:29:03 +0530

    fixedsss buggs

--- config/passport.js
+++ config/passport.js
@@ -9,8 +9,7 @@ const User = require('../models/userSchema')
 passport.use(new GoogleStrategy({
     clientID:process.env.GOOGLE_CLIENT_ID,
     clientSecret:process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: 'http://localhost:3000/auth/google/callback'
-
+    callbackURL: 'https://vanguard.today/auth/google/callback'
 ,
     
 },
