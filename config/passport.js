06586505cc91fce5271fdfe4ff5e1da43db5d154
aslam <hafizaslam@gmail.com>
2025-04-16 13:17:01 +0530

    fixed bugs

--- config/passport.js
+++ config/passport.js
@@ -10,6 +10,7 @@ passport.use(new GoogleStrategy({
     clientID:process.env.GOOGLE_CLIENT_ID,
     clientSecret:process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: 'https://vanguard.today/auth/google/callback'
+
 ,
     
 },
